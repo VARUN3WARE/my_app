07afb1e56078573a54f6eaa7f58d651b860a7f94
VARUN3WARE <106867498+VARUN3WARE@users.noreply.github.com>
2024-08-01 17:46:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,22 @@
-import streamlit as st
+import streamlit as st 
+import plotly.express as px
+st.title("hello packy poinet")
+st.header("this is my app",divider="gray")
+st.header("This is a header with a divider", divider="rainbow")
 
-st.title('🎈 App Name')
+with st.sidebar:
+  st.header("About app")
 
-st.write('Hello world!')
+df = px.data.iris()
+fig = px.scatter(
+    df,
+    x="sepal_width",
+    y="sepal_length",
+    color="species",
+    size="petal_length",
+    hover_data=["petal_width"],
+)
+
+event = st.plotly_chart(fig, key="iris", on_select="rerun")
+
+event.selection
